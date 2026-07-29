c4b13731f724f0b7b5981b4a85aa762e5efc11e0
SuhaifengEric <20188658+SuhaifengEric@users.noreply.github.com>
2026-07-29 20:50:36 +0800

    chore: install dependencies and verify workspace

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -2,7 +2,7 @@
 // 后续任务会逐步填充这些导出
 
 export * from './types/common'
-// export * from './types/models/user'
-// export * from './types/api/auth'
+export * from './types/models/user'
+export * from './types/api/auth'
 export * from './constants/enums'
 export * from './constants/labels'
