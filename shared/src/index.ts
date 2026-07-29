c2405e059a397a69bce6bff8b54d87bccbfd118d
SuhaifengEric <20188658+SuhaifengEric@users.noreply.github.com>
2026-07-29 18:12:15 +0800

    chore: add shared package structure

---
+++ shared/src/index.ts
@@ -0,0 +1,7 @@
+// 统一导出所有类型和常量
+// 将在后续步骤中填充
+export * from './types/common'
+export * from './types/models/user'
+export * from './types/api/auth'
+export * from './constants/enums'
+export * from './constants/labels'
